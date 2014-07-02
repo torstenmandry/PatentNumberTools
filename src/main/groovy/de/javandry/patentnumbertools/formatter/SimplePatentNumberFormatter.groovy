8d8788f93dbbd1a961ecc64f59dc43b920ef1e43
Torsten Mandry <torsten.mandry@opitz-consulting.com>
2014-07-02 22:27:05 +0200

    add javadoc

--- src/main/groovy/de/javandry/patentnumbertools/formatter/SimplePatentNumberFormatter.groovy
+++ src/main/groovy/de/javandry/patentnumbertools/formatter/SimplePatentNumberFormatter.groovy
@@ -2,19 +2,34 @@ package de.javandry.patentnumbertools.formatter
 
 import de.javandry.patentnumbertools.PatentNumber
 
+/**
+ * Simple PatentNumberFormatter that formats PatentNumbers based on a format String (like "ccnnnnnkd").<br/>
+ * <br/>
+ * Example<br/>
+ * <code>new SimplePatentNumberFormatter("ccyyyynnnnnnkd").format(new PatentNumber("US", "2010", "12345", "A1"))</code><br/>
+ * <br/>
+ * Use the following characters to define the pattern string:<br/>
+ * <ul>
+ *     <li>"cc" two character country code</li>
+ *     <li>"yy" or "yyyy" two or four digit year</li>
+ *     <li>"n" one digit of the numeric serial (shorter serials will be filled with leading zeros)</li>
+ *     <li>"kd" one or two characters kind code</li>
+ * </ul>
+ * @author torsten.mandry@opitz-consulting.com
+ */
 class SimplePatentNumberFormatter {
 
-    private String formatString
+    private String format
     int serialLength
 
-    SimplePatentNumberFormatter(String formatString) {
-        this.formatString = formatString
-        def matcher = formatString =~ "n+"
+    SimplePatentNumberFormatter(String format) {
+        this.format = format
+        def matcher = format =~ "n+"
         serialLength = ((String) matcher[0]).length()
     }
 
     String format(PatentNumber patentNumber) {
-        formatString
+        format
           .replace("cc", patentNumber.countryCode)
           .replace("yyyy", patentNumber.year ?: "")
           .replace("yy", patentNumber.year?.substring(2) ?: "")
